450a2d797af89c90fa803837aee5bd9e43838a41
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-19 02:46:45 -0300

    Simplify rotational_cipher python solution logic

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,13 +1,8 @@
 from string import ascii_lowercase, ascii_uppercase, maketrans, translate
 
 
-def cipher(text, from_table, to_table):
-    translation_table = maketrans(from_table, to_table)
-    return translate(text, translation_table)
-
-
 def rotate(text, key):
     to_lowercase = ascii_lowercase[key:] + ascii_lowercase[:key]
     to_uppercase = ascii_uppercase[key:] + ascii_uppercase[:key]
-    text = cipher(text, ascii_lowercase, to_lowercase)
-    return cipher(text, ascii_uppercase, to_uppercase)
+    translation_table = maketrans(ascii_lowercase + ascii_uppercase, to_lowercase + to_uppercase)
+    return translate(text, translation_table)
