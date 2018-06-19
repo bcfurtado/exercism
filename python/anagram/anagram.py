fd38d473d2229178e34b0d89eef5afd952e607bf
Bruno Furtado <brunofurtadoc@gmail.com>
2018-06-19 03:52:14 -0300

    Add anagram python solution

---
+++ python/anagram/anagram.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+
+def detect_anagrams(word, candidates):
+    counters = {
+        candidate: Counter(candidate.lower())
+        for candidate in candidates
+    }
+    word_counter = Counter(word.lower())
+    return sorted([
+        candidate for candidate, counter in counters.iteritems()
+        if word_counter in counters.values()
+        and word_counter == counter
+        and word != candidate.upper()
+    ])
